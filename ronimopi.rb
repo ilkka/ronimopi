79d202836afc181c9d63fbf6f90cb48d41504d26
Ilkka Laukkanen <ilkka.laukkanen@sasken.com>
2010-02-11 12:42:08 +0200

    Join a channel

--- ronimopi.rb
+++ ronimopi.rb
@@ -2,9 +2,16 @@
 require 'isaac/bot'
 
 bot = Isaac::Bot.new do
-  configure do
+  configure do |cfg|
+    cfg.nick = "ronimopi"
+    cfg.server = "irctr01.sasken.com"
+    cfg.port = 6667
   end
 
+  on :connect do
+    join "#vzkhgk"
+  end
+  
   on :channel do
   end
 end
