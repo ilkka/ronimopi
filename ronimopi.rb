72a028f8b8883b3cea1721d452b257ec1c974625
Ilkka Laukkanen <ilkka.laukkanen@sasken.com>
2010-02-11 13:16:11 +0200

    Allow dynamically added commands, don't die horribly on unknown ones

--- ronimopi.rb
+++ ronimopi.rb
@@ -8,11 +8,27 @@ bot = Isaac::Bot.new do
     cfg.port = 6667
   end
 
+  helpers do
+    def lounas(channel, args)
+      msg channel, "Lounas: yks kaks"
+    end
+  end
+  
   on :connect do
     join "#vzkhgk"
   end
-  
+
+  on :channel, /^[!](.+)/ do |cmdstr|
+    (cmd,args) = cmdstr.split(' ', 2)
+    if respond_to? cmd
+      send(cmd, channel, args)
+    else
+      msg channel, "I don't know that command"
+    end
+  end
+
   on :channel do
+    # catchall, do nothing
   end
 end
 
